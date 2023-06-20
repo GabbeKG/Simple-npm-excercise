8208197c1cfc227b544327f9bcdadf8c1f66834e
GabbeKG <el_sueco91@hotmail.com>
2023-06-20 20:06:12 +0200

    minor edit

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // @ts-nocheck
 
 // Import the necessary functions or objects from your package
-import { addEnterKeyListener, str } from "./node_modules/only-numbers-gabbekg";
+import {
+  addEnterKeyListener,
+  str,
+} from "./node_modules/only-numbers-gabbekg/only-numbers.js";
 addEnterKeyListener();
 
 // Use the imported function or object in your project
