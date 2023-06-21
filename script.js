a4221f8850d727164679e101ae943e329814dbe6
GabbeKG <el_sueco91@hotmail.com>
2023-06-21 20:12:39 +0200

    added webpack bundle

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-// @ts-nocheck
+import numOnly from "only-numbers-gabbekg";
 
-// Import the necessary functions from your package
-const { addEnterKeyListener, str } = require("only-numbers-gabbekg");
-
-// Use the imported functions in your project
-addEnterKeyListener();
+// Use the numOnly function
+//const input = "ABC123!@#";
+//console.log(result); // Output: "123"
 
 const btn = document.getElementById("myBtn");
-btn.addEventListener("click", str);
+btn.addEventListener("click", () => {
+  const input = document.getElementById("num").value;
+
+  const result = numOnly(input);
+  console.log(result);
+  // Perform further actions with the cleaned number
+});
